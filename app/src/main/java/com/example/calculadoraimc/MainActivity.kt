19a42156ffb08d77ccd8a0a4da88191c69f110d4
Eduardo Rezzadori <eduardo.rezzadori@inova.ind.br>
2025-07-04 14:17:50 -0300

    chore: criação do layout e do calculo

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculadoraimc.ui.theme.CalculadoraIMCTheme
 import kotlin.math.pow
-import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,13 +40,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun Home(modifier: Modifier = Modifier) {
+fun Home() {
 
-    var peso: Float = 77F
-    var altura: Float = 1.8F
-
-    var imc: Float = imcCalc(peso, altura)
-
+    var textValuePeso by remember { mutableStateOf("") }
+    var textValueAltura by remember { mutableStateOf("") }
+    var imcResult by remember { mutableStateOf("") }
+    var imc = 0F
 
     Scaffold(
         topBar = {
@@ -54,10 +62,41 @@ fun Home(modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
 
-        Text(
-            text = "Hello world! \n\n$imc",
+        Column(
             modifier = Modifier.padding(innerPadding)
-        )
+        ) {
+
+            TextField(
+                value = textValuePeso,
+                onValueChange = { newText ->
+                    textValuePeso = newText
+                },
+                label = { Text("Digite seu peso:") }
+            )
+            TextField(
+                value = textValueAltura,
+                onValueChange = { newText ->
+                    textValueAltura = newText
+                },
+                label = { Text("Digite sua altura:") }
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+
+            Button(
+                onClick = {
+                    imc = imcCalc(textValuePeso.toFloat(), textValueAltura.toFloat())
+                    imcResult = imc.toString()
+                },
+            ) {
+                Text("Calcular")
+            }
+
+            Text(
+                text = "IMC: $imcResult",
+            )
+        }
     }
 }
 
